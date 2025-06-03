4f411bd6556836d431f531be76e820d58eb32123
Brunillda Maloku <brunamaloku@gmail.com>
2025-06-03 12:09:38 +0200

    css helt klart

--- src/app/components/MovieName.jsx
+++ src/app/components/MovieName.jsx
@@ -1,6 +1,6 @@
 export default function MovieName({setTitle}) {
     return (
-        <div>
+        <div id="title-form">
             <label htmlFor="title">Titel:</label>
             <input type="text" id="title" placeholder="Titel här..." onChange={e => setTitle(e.target.value)} />
         </div>
